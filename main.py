67c183210086d788fefcbec1074489057debbbe6
Rehan-shah <Rehansnehalshah1@gmail.com>
2023-08-06 10:53:47 +0530

    Making deploy ready

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import listdir
 from flask import Flask , request
 import time
 from selenium.webdriver.common.by import By
@@ -6,22 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pyperclip
 app = Flask(__name__)
+import uuid
+
+list = {}
 
 
-driver = webdriver.Edge()
-
-
-
-driver.get("https://gpt-gm.h2o.ai/")
-
-
-login_btn = driver.find_element(By.XPATH , '//*[@id="app"]/div/div[1]/div/div/div/form/button')
-login_btn.click()
-
-
-
-
-def send_prompts(prompt):
+def send_prompts(prompt , driver):
     print(prompt)
     time.sleep(5)
 
@@ -41,23 +32,79 @@ def send_prompts(prompt):
         except:
             break;
 
-    element = driver.find_elements(By.XPATH , "//div[@class='prose max-w-none dark:prose-invert max-sm:prose-sm prose-headings:font-semibold prose-h1:text-lg prose-h2:text-base prose-h3:text-base prose-pre:bg-gray-800 dark:prose-pre:bg-gray-900']")
-    print("done") 
-    return element[-1].text
+    elements = driver.find_elements(By.XPATH , "//div[@class='prose max-w-none dark:prose-invert max-sm:prose-sm prose-headings:font-semibold prose-h1:text-lg prose-h2:text-base prose-h3:text-base prose-pre:bg-gray-800 dark:prose-pre:bg-gray-900']")
+    text = elements[-1].text
+
+    try :
+        code = driver.find_elements(By.TAG_NAME , "code")
+        print("jell")
+        code_text = code[-1].text
+        code_attr = code[-1].get_attribute("class")
+        text = text.replace(code_text , f"{code_attr}```" + code_text + "```")
+
+    finally:
+        return text
 
 
 
+@app.get('/start')
+def start(): 
+    driver = webdriver.Edge()
+    driver.get("https://gpt-gm.h2o.ai/")
+    login_btn = driver.find_element(By.XPATH , '//*[@id="app"]/div/div[1]/div/div/div/form/button')
+    login_btn.click()
+    id = uuid.uuid1()
+    list[str(id)] = driver
+    print(list)
+    return {"id" : id}, 200
 
 
-@app.post("/chatgpt")
+
+@app.post("/chat")
 def chatgpt():
+    data = request.json
+    print(list)
+    try : 
+        prompt = data["prompt"]
+        id = data["id"]
+    except KeyError:
+        return {"error": "No prompt provided"}, 400
+
+    try : 
+        driver = list[id]
+    except KeyError:
+        return {"error": "Thread not on , pls add a thread"}, 400
+
+    return {"message": send_prompts(prompt , driver=driver)}, 200
+
+@app.post("/stop")
+def stop():
+    data = request.json
+    try : 
+        id = data["id"]
+        driver = list[id]
+    except KeyError:
+        return {"error": "Not a eligible id provided"}, 400
+    driver.quit()
+    del list[id]
+    return {"message": "Thread stopped"}, 200
+
+@app.post("/instruct")
+def instrcut():
     data = request.json
     try : 
         prompt = data["prompt"]
     except KeyError:
         return {"error": "No prompt provided"}, 400
+    driver = webdriver.Edge()
+    driver.get("https://gpt-gm.h2o.ai/")
+    login_btn = driver.find_element(By.XPATH , '//*[@id="app"]/div/div[1]/div/div/div/form/button')
+    login_btn.click()
+    msg = send_prompts(prompt , driver)
+    driver.quit()
+    return {"message": msg}, 200
+    
 
-    return {"message": send_prompts(prompt)}, 200
 
 @app.errorhandler(404) 
 def invalid_route(e): 
